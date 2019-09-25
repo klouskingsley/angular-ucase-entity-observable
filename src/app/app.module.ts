001c634ffbdbb8c21ee766237746980d33d863b9
klouskingsley <harry_tse@163.com>
2019-09-25 17:44:59 +0800

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CommentComponent } from './comment/comment.component'
-import { AComponent } from './a/a.component'
+import { DefaultStrategyComponent } from './default-strategy/default-strategy.component'
 
 import {CommentUcase} from './comment/comment.ucase'
 import {CommentWsEntity} from './comment/comment-ws.entity'
--- src/app/default-strategy/default-strategy.component.ts
+++ src/app/default-strategy/default-strategy.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, OnInit, OnDestroy, NgZone } from '@angular/core';
 
 @Component({
-  selector: 'app-a',
+  selector: 'app-default-strategy',
   template: `
     <div style="border: 1px solid red; margin: 10px;">component a</div>
   `,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class AComponent {
+export class DefaultStrategyComponent {
   get isRendered () {
     console.log('component a rendered')
     return false
